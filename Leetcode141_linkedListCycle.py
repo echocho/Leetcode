b21b1c8eb3a46751f27afa1094a015124091756c
Zihui Zheng <zihui@example.com>
2018-06-17 00:35:30 +0800

    Delete Solution 2. Add time complexity and explanation.

--- Leetcode141_linkedListCycle.py
+++ Leetcode141_linkedListCycle.py
@@ -6,30 +6,26 @@
 #         self.val = x
 #         self.next = None
 
-def hasCycle(self, head):
+def hasCycle(head):
     """
     :type head: ListNode
     :rtype: bool
     """
+    # Solution 1:
+    # iterate and put head in a hashset
+    # if there is a cycle, we will have a duplicate element
+    # meaning the node we are pointing to is the same as the one in the set
+
+    # O(n)
+
     if head == None: return False
     nodesChecked = set()
     while head:
-        if head in aSet:
+        if head in nodesChecked:
             return True
         nodesChecked.add(head)
         head = head.next
     return False
-
-    # Solution 2
-    if head == None: return False
-    fastPointer = head.next
-    slowPointer = head
-    while slowPointer != fastPointer:
-        if slowPointer == None or fastPointer == None:
-            return False
-        slowPointer = slowPointer.next
-        fastPointer = fastPointer.next.next
-    return True
 
 
 
