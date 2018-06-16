8e2e5fc6c33e7be18281dbb6c1e7ffa7bb987738
Zihui Zheng <zihui@example.com>
2018-06-17 00:19:39 +0800

    fixed solution. Time complexity O(N**2).

--- Leetcode15_threeSum.py
+++ Leetcode15_threeSum.py
@@ -1,23 +1,47 @@
 # Leetcode15_threeSum.py
 
-import itertools
 
-def threeSum(nums):
-    """
-    :type nums: List[int]
-    :rtype: List[List[int]]
-    """
-    # Solution 1: iterate and get all possible sub arrays
-    # O(n**2)
-    ans = []
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            ans.append([nums[i], nums[j], nums[j]])
+# Solution 1: similar to 2sum. Use -nums[i] as target instead and find elements that
+# add up to target
+# use left and right pointers to locate the elements being added up and compare
 
-    return [elem for elem in ans if sum(elem) == 0]
+# O(n**2)
+
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        ans = []
+        for i in range(len(nums)-2):
+            # print('i: ', i)
+            if i == 0 or nums[i] > nums[i-1]:
+                left = i + 1; right = len(nums) - 1
+                # print('left: ', left, 'right', right)
+                while left < right:
+                    if nums[left] + nums[right] == -nums[i]:
+                        # print('found it sum of -sum[i]:', -nums[i])
+                        # print('num[left]', nums[left], ';', 'num[right]', nums[right])
+                        ans.append([nums[i], nums[left], nums[right]])
+                        left += 1; right -= 1
+                        while left < right and nums[left] == nums[left-1]: # leave out duplicate numbers
+                            left +=1
+                        while left < right and nums[right] == nums[right+1]: # leave out duplicate numbers
+                            right -= 1
+                    elif nums[left] + nums[right] < -nums[i]: # if sum is smaller than target, increment left pointer
+                        while left < right:
+                            left += 1
+                            break
+                    else:
+                        while left < right: # if sum is larger than target, decrement right pointer
+                            right -= 1
+                            break
+        return ans
 
 
-print(threeSum([-1, 0, 1, 2, -1, -4]) == [[-1,-1,2],[-1,0,1]])
+print(Solution().threeSum([-1, 0, 1, 2, -1, -4]) == [[-1,-1,2],[-1,0,1]])
 
 
 
