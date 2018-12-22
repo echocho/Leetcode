3ad2f779d252411a3a2257097b748a8c793abdce
echocho <echo333cheng@gmail.com>
2018-12-22 14:57:53 +0800

    solution

--- no665_NondecreasingArray.py
+++ no665_NondecreasingArray.py
@@ -1,1 +1,17 @@
-def 
+def checkPossibility(nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    if not nums:
+        return True
+    
+    modified = False
+    for i in range(1, len(nums)):
+        if nums[i] < nums[i-1]:
+            if modified:
+                return False
+            if i >=2 and nums[i] < nums[i-2]:
+                nums[i] = nums[i-1]
+            modified = True
+    return True
