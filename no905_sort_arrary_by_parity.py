b4021f64323c934e72fbb89b7767e42dc1569ac5
echocho <echo333cheng@gmail.com>
2018-12-23 22:47:09 +0800

    added in-place quick sort solution

--- no905_sort_arrary_by_parity.py
+++ no905_sort_arrary_by_parity.py
@@ -22,3 +22,19 @@ def sortArrayByParity(A):
     """
     A.sort(key=lambda x: x%2)
     return A
+
+#solution3: inplace; use quick sort algorithm
+def sortArrayByParity(A):
+    left, right = 0, len(A) -1 
+    while left < right:
+        if A[left] % 2 > A[right] % 2:
+            A[left], A[right] = A[right], A[left]
+        if A[left] % 2 == 0:
+            left += 1
+        if A[right] % 2 == 1:
+            right -= 1
+    return A
+print(sortArrayByParity([3,1,2,4]))
+print(sortArrayByParity([1]))
+print(sortArrayByParity([1,0,3]))
+print(sortArrayByParity([0,1,2]))
