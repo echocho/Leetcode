e0a728128a4a672ff1cfeaca890ace2492f04646
echocho <echo333cheng@gmail.com>
2018-12-26 21:29:16 +0800

    added solution 2

--- no922_sort_array_by_parity_ii.py
+++ no922_sort_array_by_parity_ii.py
@@ -1,3 +1,4 @@
+# solution1: extra space
 def sortArrayByParityII(A):
     """
     :type A: List[int]
@@ -15,4 +16,22 @@ def sortArrayByParityII(A):
         output.append(odds[i])
     return output
 
+# print(sortArrayByParityII([4,7,2,5,6,9]))
+
+# solution2: inplace - make each A[i%2==0] is even
+def sortArrayByParityII(A):
+    """
+    :type A: List[int]
+    :rtype: List[int]
+    """
+    j = 1
+    for i in range(0, len(A), 2):
+        if A[i] % 2:
+            while A[j] % 2:
+                j += 2
+            A[i], A[j] = A[j], A[i]
+    return A
+print(sortArrayByParityII([1,3,5,2,4,6]))    
 print(sortArrayByParityII([4,7,2,5,6,9]))
+
+
