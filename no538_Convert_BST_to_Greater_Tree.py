a6c305d7b96a46fe3503e4165cb182ae38d239fc
echocho <echo333cheng@gmail.com>
2019-01-08 00:20:25 +0800

    now work on solution 2: reverse inorder tree traversal

---
+++ no538_Convert_BST_to_Greater_Tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution1:
+        # iterate the tree twice
+        # first time: get all vals 
+        # second time: modify node.vals
+        # NOT EFFICIENT ENOUGH THOUGH
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        
+        if not root:
+            return None
+        vals, stack = [], [root,]
+        # first time
+        while stack:
+            node = stack.pop()
+            vals.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        # second time
+        stack = [root,]
+        while stack:
+            node = stack.pop()
+            node.val += sum([i for i in vals if i > node.val])
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
+
+class Solution2:
+    # iterative reverse in order tree traversal
