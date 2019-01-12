af50642b308a410c20aa15f40c232edc28d9a340
echocho <echo333cheng@gmail.com>
2019-01-12 14:29:01 +0800

    solution

--- no257_Binary_Tree_Path.py
+++ no257_Binary_Tree_Path.py
@@ -1,2 +1,27 @@
-cdeabcdeab
-abcde
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root:
+        	return []
+        
+        paths = []
+        stack = [(root, str(root.val))]
+        while stack:
+        	node, path = stack.pop()
+        	if node.left:
+        		stack.append((node.left, path + '->' + str(node.left.val)))
+        	if node.right:
+        		stack.append((node.right, path + '->' + str(node.right.val)))
+        	if not node.left and not node.right:
+        		paths.append(path)
+        return paths
