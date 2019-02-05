ceebdde1b414d1ac4b54a01ee97e4531d3c13855
echocho <echo333cheng@gmail.com>
2019-02-05 16:07:27 +0800

    solution

---
+++ no707_DesignLinkedList.py
@@ -0,0 +1,70 @@
+class MyLinkedList:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.lst = []
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        try:
+            return self.lst[index]
+        except:
+            return -1
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        self.lst = [val] + self.lst
+        
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        self.lst += [val]
+        
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+        if index > len(self.lst):
+            return 
+        if index == len(self.lst):
+            self.lst += [val]
+            return
+        self.lst = self.lst[:index] + [val] + self.lst[index:]
+
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
+        try:
+            self.lst = self.lst[:index] + self.lst[index + 1 :]
+        except:
+            pass
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
