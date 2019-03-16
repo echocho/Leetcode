2777b8ef5d0811d378e8a2539b9020d2530131b0
echocho <echo333cheng@gmail.com>
2019-03-16 17:59:09 +0800

    solution

--- no36_valid_sudoku.py
+++ no36_valid_sudoku.py
@@ -1,5 +1,37 @@
-from collections import Counter
-sodoku = [
+class Solution:
+	def has_duplicate_number(self, lst): 
+		num_set = set()
+		for item in lst:
+			if item != '.':
+				if item in num_set:
+					return True
+				num_set.add(item)
+		return False
+
+	def row_is_valid(self, board):
+		for row in board:
+			if self.has_duplicate_number(row):
+				return False
+		return True
+
+	def col_is_valid(self, board):
+		for col in zip(*board):
+			if self.has_duplicate_number(col):
+				return False
+		return True 
+
+	def sub_box_is_valid(self, board):
+		for i in (0, 3, 6):
+			for j in (0, 3, 6):
+				sub_box = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+				if self.has_duplicate_number(sub_box):
+					return False
+		return True
+
+	def isValidSudoku(self, board):
+		return self.row_is_valid(board) and self.col_is_valid(board) and self.sub_box_is_valid(board)
+
+test1 = [
   ["8","3",".",".","7",".",".",".","."],
   ["6",".",".","1","9","5",".",".","."],
   [".","9","8",".",".",".",".","6","."],
@@ -10,23 +42,26 @@ sodoku = [
   [".",".",".","4","1","9",".",".","5"],
   [".",".",".",".","8",".",".","7","9"]
 ]
-
-def has_duplicate_number(dictionary):
-	if any([v > 1 for k, v in digits.items() if k != '.']):
-		return True
-	return False
-
-for row in sodoku:
-	digits = Counter(row)
-	if has_duplicate_number(digits):
-		print('row wrong')
-
-for col in zip(*sodoku):
-	digits = Counter(col)
-	if has_duplicate_number(digits):
-		print('col wrong')
-
-start, end = 0, 9
-cnt_left = 3
-# while i <= 9 and j <= 9:
-sub_box = [sodoku[i][j] for i in range(0, )]
+print(Solution().isValidSudoku(test1) == False)
+test2 = [
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+print(Solution().isValidSudoku(test2) == True)
+test3 = [[".",".",".",".","5",".",".","1","."],
+         [".","4",".","3",".",".",".",".","."],
+         [".",".",".",".",".","3",".",".","1"],
+         ["8",".",".",".",".",".",".","2","."],
+         [".",".","2",".","7",".",".",".","."],
+         [".","1","5",".",".",".",".",".","."],
+         [".",".",".",".",".","2",".",".","."],
+         [".","2",".","9",".",".",".",".","."],
+         [".",".","4",".",".",".",".",".","."]]
+print(Solution().isValidSudoku(test3) == False)
