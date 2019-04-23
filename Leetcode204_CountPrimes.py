30422b039b23237ad116958fcdfc8ff1c0104484
echocho <echo333cheng@gmail.com>
2019-04-23 14:33:45 +0800

    optimized solution

--- Leetcode204_CountPrimes.py
+++ Leetcode204_CountPrimes.py
@@ -1,7 +1,8 @@
 import math
+import timeit
 
-class Solution(object):
-    import math
+class Solution1:
+    # Time complexity too high
     def isPrime(self, num):
         if num < 2:
             return False
@@ -9,7 +10,7 @@ class Solution(object):
             if num % i == 0:
                 return False
         return True
-    
+
     def countPrimes(self, n):
         """
         :type n: int
@@ -23,4 +24,19 @@ class Solution(object):
                 cnt += 1
         return cnt
 
-print(Solution().countPrimes(1500000))
+class Solution2:
+    def countPrimes(self, n):
+        if n < 3:
+            return 0
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                primes[i * i : n : i] = [False] * len(primes[i * i : n : i])
+        return sum(primes)
+
+print('input 1500000')
+print('timing solution1')
+print(timeit.Timer(lambda: Solution1().countPrimes(1500000)).timeit(number=1))
+print('timing solution2')
+print(timeit.Timer(lambda: Solution2().countPrimes(1500000)).timeit(number=1))
