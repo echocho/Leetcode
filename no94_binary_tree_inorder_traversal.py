143707c6ec3bba8b5292263f050cc2e9dd71782c
echocho <echo333cheng@gmail.com>
2019-11-03 21:33:55 +0800

    mock call stack

--- no94_binary_tree_inorder_traversal.py
+++ no94_binary_tree_inorder_traversal.py
@@ -17,3 +17,31 @@ Class SolutionRecursion:
         self.helper(root.left, res)
         res.append(root.val)
         self.helper(root.right, res)
+
+Class SolutionCallStack:
+    def inorder_traversal(self, root):
+        call_stack = []
+        vals = []
+        next_exec = 'CHECK_NULL'
+        while next_exec or len(call_stack):
+            if next_exec == 'CHECK_NULL':
+                if not root:
+                    if not call_stack:
+                        break
+                    root, next_exec = call_stack.pop()
+                else:
+                    next_exec = 'LEFT'
+            elif next_exec == 'LEFT':
+                call_stack.append((root, 'APPEND_VAL'))
+                root = root.left
+                next_exec = 'CHECK_NULL'
+            elif next_exec == 'APPEND_VAL':
+                vals.append(root.val)
+                next_exec = 'RIGHT'
+            elif next_exec == 'RIGHT':
+                call_stack.append((root, None))
+                root = root.right
+                next_exec = 'CHECK_NULL'
+            else:
+                root, next_exec = call_stack.pop()             
+        return vals
